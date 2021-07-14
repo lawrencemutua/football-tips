fbcad31c65a240d6045fce30c4aefb04f0d3f2f1
lawrencemutua <lawrencemutua99@gmail.com>
2021-07-14 22:08:32 +0300

    jackpot

--- app/src/main/java/com/example/myapplication/JackpotTips.java
+++ app/src/main/java/com/example/myapplication/JackpotTips.java
@@ -10,5 +10,6 @@ public class JackpotTips extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jackpottips);
+
     }
 }
